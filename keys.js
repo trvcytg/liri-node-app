37e320090f703710084d435910081a7b495c363c
Todd Tracy <trvcytg@gmail.com>
2019-10-05 13:57:38 -0500

    Hid omdb API key and Seatgeek Client ID

--- keys.js
+++ keys.js
@@ -4,3 +4,5 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+exports.seatgeekClientID = process.env.SEATGEEKCLIENT_ID;
+exports.omdbKey = process.env.OMDB_KEY;
